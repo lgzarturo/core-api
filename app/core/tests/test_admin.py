54f6c19049a669821bb9d55adfd8309d62c4c285
Arturo López <lgzarturo@gmail.com>
2020-03-10 10:10:00 -0500

    Soporte para el administrador de usuarios

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,34 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com', password='password#123')
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@gmail.com', password='password$123',
+            name='Test user full name')
+
+    def test_users_listed(self):
+        """Probando que los usuarios se muestren en la página"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Prueba para editar usuarios"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Probar la pagina de crear usuario"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
