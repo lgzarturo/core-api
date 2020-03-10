f4e50ad1a327b0aff18af0ff160b9c9c7f351533
Arturo López <lgzarturo@gmail.com>
2020-03-10 17:39:55 -0500

    fix identación

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -97,4 +97,3 @@ class PublicUserApiTest(TestCase):
         res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        
